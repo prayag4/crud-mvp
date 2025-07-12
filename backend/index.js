88464afdaa06bf11493d9af7f5e672519c9666e8
prayag jain <prayag6547@gmail.com>
2025-07-12 16:52:33 +0530

    port changes for render

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const recordRoutes = require('./routes/recordRoutes');
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,6 +16,9 @@ app.get('/', (req, res) => {
 // ✅ Use Record Routes
 app.use('/api/records', recordRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
